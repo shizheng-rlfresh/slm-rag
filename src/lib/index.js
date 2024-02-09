d0df9528895dee1f83fa8466e13bc51aab6e1d74
Zheng Shi <shi.zheng.tfls@gmail.com>
2024-02-08 22:24:55 -0500

    add svelte component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,46 +1,44 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { AutoTokenizer, pipeline } from '@xenova/transformers';
 
-// Create text-generation pipeline
-const pipe = await pipeline('text-generation', 'shi-zheng-qxhs/gpt2_oasst2_curated_onnx');
-const tokenizer = await AutoTokenizer.from_pretrained('shi-zheng-qxhs/gpt2_oasst2_curated_onnx');
+/**
+ * Generates a response from the AI assistant based on the user's message.
+ *
+ * @param {string} userMessage - The message input by the user
+ * @return {Promise<string>} The generated response from the AI assistant
+ */
+export async function chatGenerate(userMessage) {
+    const textGenerationPipeline = await pipeline(
+        'text-generation',
+        'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
+    );
+    const tokenizer = await AutoTokenizer.from_pretrained(
+        'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
+    );
 
-let message = 'Who is the president of the United States?';
-message = [
-    { content: 'Below is a dialogue between a human user and an AI assistant.', role: 'system' },
-    { content: message, role: 'user' },
-];
-message = tokenizer.apply_chat_template(message, { tokenize: false }) + '<|assistant|>';
+    const systemContent =
+        'Below is a dialogue between a human user and an very helpful AI assistant named CuteChat.\n';
 
-const output = await pipe(message);
-console.log(output[0].generated_text);
+    let message = [
+        {
+            content: systemContent,
+            role: 'system',
+        },
+        { content: userMessage, role: 'user' },
+    ];
 
-// console.log(message);
+    message = tokenizer.apply_chat_template(message, { tokenize: false }) + '<|assistant|>';
 
-// message = tokenizer(message, { return_tensors: 'pt' });
-// console.log(message);
+    const output = await textGenerationPipeline(message, {
+        max_new_tokens: 128,
+        penalty_alpha: 0.6,
+        top_k: 6,
+        eos_token_id: tokenizer.eos_token_id,
+        pad_token_id: tokenizer.pad_token_id,
+    });
 
-// const output = await model.generate(message);
-// console.log(output[0].generated_text);
+    return output[0].generated_text.replace(systemContent, '').replace(userMessage, '').trim();
+}
 
-// // Define the prompt and list of messages
-// const prompt = 'Give me a short introduction to large language model.';
-// const messages = [
-//     { role: 'system', content: 'You are a helpful assistant.' },
-//     { role: 'user', content: prompt },
-// ];
-
-// // Apply chat template
-// const text = generator.tokenizer.apply_chat_template(messages, {
-//     tokenize: false,
-//     add_generation_prompt: true,
-
-// });
-
-// // Generate text
-// const output = await generator(text, {
-//     max_new_tokens: 128,
-//     do_sample: false,
-// });
-// console.log(output[0].generated_text);
+// generate('Who is the president of People Republic of China').then((output) => console.log(output));
 // // 'A large language model is a type of artificial intelligence system that can generate text based on the input provided by users, such as books, articles, or websites. It uses advanced algorithms and techniques to learn from vast amounts of data and improve its performance over time through machine learning and natural language processing (NLP). Large language models have become increasingly popular in recent years due to their ability to handle complex tasks such as generating human-like text quickly and accurately. They have also been used in various fields such as customer service chatbots, virtual assistants, and search engines for information retrieval purposes.'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
+<script>
+    import { chatGenerate } from '$lib/index';
+
+    let userMessage = '';
+    let generatedText = '';
+
+    async function generateReponse() {
+        if (userMessage) {
+            try {
+                generatedText = await chatGenerate(userMessage);
+                console.log(generatedText);
+            } catch (error) {
+                console.error('Error: generating response', error);
+                generatedText = 'An error occurred while generating the response.';
+            }
+        }
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input type="text" bind:value={userMessage} placeholder="Type your message here..." />
+<button on:click={generateReponse}>Generate Response</button>
+
+{#if generatedText}
+    <p class="generated-response">{generatedText}</p>
+{/if}
