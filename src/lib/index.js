729be0d71224577a88cc88c58e6ea2f7ac181656
Zheng Shi <shi.zheng.tfls@gmail.com>
2024-02-09 21:00:40 -0500

    update svelte page

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,17 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { AutoTokenizer, pipeline } from '@xenova/transformers';
 
-/**
- * Generates a response from the AI assistant based on the user's message.
- *
- * @param {string} userMessage - The message input by the user
- * @return {Promise<string>} The generated response from the AI assistant
- */
-export async function chatGenerate(userMessage) {
-    const textGenerationPipeline = await pipeline(
-        'text-generation',
-        'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
-    );
+export async function chatTempalte(userMessage) {
     const tokenizer = await AutoTokenizer.from_pretrained(
         'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
     );
@@ -29,6 +19,37 @@ export async function chatGenerate(userMessage) {
 
     message = tokenizer.apply_chat_template(message, { tokenize: false }) + '<|assistant|>';
 
+    return message;
+}
+
+/**
+ * Generates a response from the AI assistant based on the user's message.
+ *
+ * @param {string} userMessage - The message input by the user
+ * @return {Promise<string>} The generated response from the AI assistant
+ */
+export async function chatGenerate(userMessage) {
+    const textGenerationPipeline = await pipeline(
+        'text-generation',
+        'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
+    );
+    // const tokenizer = await AutoTokenizer.from_pretrained(
+    //     'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
+    // );
+
+    // const systemContent =
+    //     'Below is a dialogue between a human user and an very helpful AI assistant named CuteChat.\n';
+
+    // let message = [
+    //     {
+    //         content: systemContent,
+    //         role: 'system',
+    //     },
+    //     { content: userMessage, role: 'user' },
+    // ];
+
+    // message = tokenizer.apply_chat_template(message, { tokenize: false }) + '<|assistant|>';
+
     const output = await textGenerationPipeline(message, {
         max_new_tokens: 128,
         penalty_alpha: 0.6,
@@ -37,8 +58,47 @@ export async function chatGenerate(userMessage) {
         pad_token_id: tokenizer.pad_token_id,
     });
 
-    return output[0].generated_text.replace(systemContent, '').replace(userMessage, '').trim();
+    return output[0].generated_text.replace(userMessage, '').trim();
 }
 
-// generate('Who is the president of People Republic of China').then((output) => console.log(output));
+// /**
+//  * Generates a response from the AI assistant based on the user's message.
+//  *
+//  * @param {string} userMessage - The message input by the user
+//  * @return {Promise<string>} The generated response from the AI assistant
+//  */
+// export async function chatGenerate(userMessage) {
+//     const textGenerationPipeline = await pipeline(
+//         'text-generation',
+//         'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
+//     );
+//     const tokenizer = await AutoTokenizer.from_pretrained(
+//         'shi-zheng-qxhs/gpt2_oasst2_curated_onnx'
+//     );
+
+//     const systemContent =
+//         'Below is a dialogue between a human user and an very helpful AI assistant named CuteChat.\n';
+
+//     let message = [
+//         {
+//             content: systemContent,
+//             role: 'system',
+//         },
+//         { content: userMessage, role: 'user' },
+//     ];
+
+//     message = tokenizer.apply_chat_template(message, { tokenize: false }) + '<|assistant|>';
+
+//     const output = await textGenerationPipeline(message, {
+//         max_new_tokens: 128,
+//         penalty_alpha: 0.6,
+//         top_k: 6,
+//         eos_token_id: tokenizer.eos_token_id,
+//         pad_token_id: tokenizer.pad_token_id,
+//     });
+
+//     return output[0].generated_text.replace(systemContent, '').replace(userMessage, '').trim();
+// }
+
+// chatGenerate('Who is the president of the United States?').then((output) => console.log(output));
 // // 'A large language model is a type of artificial intelligence system that can generate text based on the input provided by users, such as books, articles, or websites. It uses advanced algorithms and techniques to learn from vast amounts of data and improve its performance over time through machine learning and natural language processing (NLP). Large language models have become increasingly popular in recent years due to their ability to handle complex tasks such as generating human-like text quickly and accurately. They have also been used in various fields such as customer service chatbots, virtual assistants, and search engines for information retrieval purposes.'
---
+++ src/routes/chat2.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { onMount } from 'svelte';
+    import { createEventDispatcher } from 'svelte';
+    import { chatTempalte, chatGenerate } from '$lib';
+    import { DeepChat } from 'deep-chat';
+    // import { DeepChat } from 'deep-chat';
+    // Make sure to use onMount and render deep-chat on load
+    onMount(async () => {
+        await import('deep-chat');
+        isLoaded = true;
+    });
+
+    let isLoaded = false;
+
+    // const initialMessages = [{ content: introMessage, role: 'assistant' }];
+
+    const dispatch = createEventDispatcher();
+    const dispatchNewMessage = (event) => {
+        dispatch('new-message', event);
+    };
+
+    let userMessage = '';
+</script>
+
+<main>
+    <h1>Deep Chat</h1>
+    {#if isLoaded}
+        <!-- demo/textInput are examples of passing an object directly into a property -->
+        <!-- initialMessages is an example of passing an object from script into a property -->
+        <deep-chat
+            on:new-message={dispatchNewMessage}
+            demo="true"
+            introMessage={{
+                text: 'Hi, my name is CuteChat. I am a fine-tuned gpt2-small on OASST2 with a NVIDIA Tesla T4 GPU.',
+            }}
+            directConnection={{
+                huggingFace: {
+                    key: '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',
+                    textGeneration: {
+                        model: 'shi-zheng-qxhs/gpt2_oasst2_curated',
+                        parameters: {
+                            max_new_tokens: 128,
+                            penalty_alpha: 0.6,
+                            top_k: 6,
+                            eos_token_id: 50256,
+                            pad_token_id: 50256,
+                        },
+                    },
+                },
+            }}
+            requestInterceptor={async (details) => {
+                details.body.inputs = await chatTempalte(details.body.inputs);
+                userMessage = details.body.inputs;
+                return details;
+            }}
+            responseInterceptor={async (details) => {
+                details[0].generated_text = details[0].generated_text
+                    .replace(userMessage, '')
+                    .trim();
+                return details;
+            }}
+        />
+    {/if}
+</main>
+
+<style>
+    main {
+        font-family: sans-serif;
+        text-align: center;
+        justify-content: center;
+        display: grid;
+    }
+</style>
