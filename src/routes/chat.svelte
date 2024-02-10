bffcb76ba22098356efc0b341e7b230d25d41790
Zheng Shi <shi.zheng.tfls@gmail.com>
2024-02-10 17:37:30 -0500

    update demo png file

--- src/routes/chat.svelte
+++ src/routes/chat.svelte
@@ -22,41 +22,43 @@
 
 <main class="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
     <h1
-        class="mb-6 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-4xl font-bold text-transparent"
+        class="mb-0 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-4xl font-bold text-transparent"
     >
         CuteChat Demo
     </h1>
     {#if isLoaded}
-        <deep-chat
-            demo="true"
-            introMessage={{
-                text: introMessage,
-            }}
-            request={{
-                handler: async (body, signals) => {
-                    try {
-                        // Generating chat response
-                        const text = await chatGenerate(body.messages[0].text);
-                        signals.onResponse({ text: text[0].generated_text });
-                    } catch (e) {
-                        // Handling errors
-                        console.error(e);
-                        signals.onResponse({ text: 'Failed to process pipeline' });
-                    }
-                },
-            }}
-            requestInterceptor={async (request) => {
-                // Intercepting and processing user message
-                userMessage = await chatTempalte(request.body.messages[0].text);
-                request.body.messages[0].text = userMessage.encoded_message;
-                return request;
-            }}
-            responseInterceptor={async (response) => {
-                // Manipulating response to remove user message from it
-                response.text = response.text.replace(userMessage.decoded_message, '');
-                return response;
-            }}
-        />
+        <div class="-mx-4 w-full max-w-md rounded-lg bg-white shadow-md">
+            <deep-chat
+                demo="true"
+                introMessage={{
+                    text: introMessage,
+                }}
+                request={{
+                    handler: async (body, signals) => {
+                        try {
+                            // Generating chat response
+                            const text = await chatGenerate(body.messages[0].text);
+                            signals.onResponse({ text: text[0].generated_text });
+                        } catch (e) {
+                            // Handling errors
+                            console.error(e);
+                            signals.onResponse({ text: 'Failed to process pipeline' });
+                        }
+                    },
+                }}
+                requestInterceptor={async (request) => {
+                    // Intercepting and processing user message
+                    userMessage = await chatTempalte(request.body.messages[0].text);
+                    request.body.messages[0].text = userMessage.encoded_message;
+                    return request;
+                }}
+                responseInterceptor={async (response) => {
+                    // Manipulating response to remove user message from it
+                    response.text = response.text.replace(userMessage.decoded_message, '');
+                    return response;
+                }}
+            />
+        </div>
     {/if}
 </main>
 
