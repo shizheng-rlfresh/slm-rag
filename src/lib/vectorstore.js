7630e9b2e55fd3e92e985c78eb3571cdef53cdde
Zheng Shi <shi.zheng.tfls@gmail.com>
2024-02-12 21:35:11 -0500

    fix PDFJS.workSrc by manually setting it, and fixed vite top-level-await by editing vite.config.js files

--- src/lib/vectorstore.js
+++ src/lib/vectorstore.js
@@ -1,29 +1,84 @@
 import { WebPDFLoader } from 'langchain/document_loaders/web/pdf';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-// import { HuggingFaceTransformersEmbeddings } from '@langchain/community/embeddings/hf_transformers';
-// import { CloseVectorWeb } from '@langchain/community/vectorstores/closevector/web';
+const pdfjs = await import('pdfjs-dist/legacy/build/pdf.min.mjs');
+const pdfjsWorker = await import('pdfjs-dist/legacy/build/pdf.worker.min.mjs');
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
-export async function prepareDocs(url, args) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    const blob = await response.blob();
-    const loader = new WebPDFLoader(blob);
+const pdfUrl = './src/lib/assets/part_time_phd.pdf';
 
-    try {
-        const pages = await loader.load();
-        console.log(typeof pages);
-        const text_splitter = new RecursiveCharacterTextSplitter({
-            chunkSize: args.chunkSize ? args.chunkSize : 1000,
-            chunkOverlap: args.chunkOverlap ? args.chunkOverlap : 200,
-        });
-        const docs = await text_splitter.splitDocuments(pages);
-        return docs;
-    } catch (error) {
-        console.log('Error loading or splitting document: ', error);
-    }
+export async function prepareDocs(pdfUrl) {
+    const pdfResponse = await fetch(pdfUrl);
+    const pdfBlob = await pdfResponse.blob();
+    const pdfLoader = new WebPDFLoader(pdfBlob, {
+        pdfjs: () => Promise.resolve(pdfjs),
+    });
+    const docs = await pdfLoader.load();
+
+    return docs;
 }
+
+// export function test() {
+//     prepareDocs(pdfUrl).then((loader) => {
+//         docs = loader.load();
+//         console.log(docs);
+//     });
+// }
+
+// import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+// // import { HuggingFaceTransformersEmbeddings } from '@langchain/community/embeddings/hf_transformers';
+// // import { CloseVectorWeb } from '@langchain/community/vectorstores/closevector/web';
+
+// pdfjs.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/legacy/build/pdf.worker.js';
+
+// export async function test() {
+//     const blob = new Blob();
+//     const loader = new WebPDFLoader(blob, {
+//         pdfjs: () => Promise.resolve(pdfjs),
+//     });
+//     const pages = await loader.load();
+//     console.log(pages);
+// }
+
+// export async function test() {
+//     const blob = new Blob();
+//     const loader = new WebPDFLoader(blob, {
+//         pdfjs: () => import('pdfjs-dist/legacy/build/pdf.mjs'),
+//     });
+//     const pages = await loader.load();
+//     console.log(pages);
+// }
+
+// import { WebPDFLoader } from 'langchain/document_loaders/web/pdf';
+// // import pdfjs from 'pdfjs-dist/legacy/build/pdf.mjs';
+
+// const blob = new Blob(); // e.g. from a file input
+
+// // pdfjs.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/legacy/build/pdf.worker.mjs';
+
+// const loader = new WebPDFLoader(blob, {
+//     pdfjs: () => import('pdfjs-dist/legacy/build/pdf.mjs'),
+// });
+
+// export async function prepareDocs(url, args) {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//     }
+//     const blob = await response.blob();
+//     const loader = new WebPDFLoader(blob);
+
+//     try {
+//         const pages = await loader.load();
+//         console.log(typeof pages);
+//         const text_splitter = new RecursiveCharacterTextSplitter({
+//             chunkSize: args.chunkSize ? args.chunkSize : 1000,
+//             chunkOverlap: args.chunkOverlap ? args.chunkOverlap : 200,
+//         });
+//         const docs = await text_splitter.splitDocuments(pages);
+//         return docs;
+//     } catch (error) {
+//         console.log('Error loading or splitting document: ', error);
+//     }
+// }
 
 // export async function createVectorStore(url, args) {
 //     try {
--- src/routes/rag/rag.svelte
+++ src/routes/rag/rag.svelte
@@ -1,27 +1,32 @@
 <script>
-    import { prepareDocs } from '$lib/vectorstore';
     // import { WebPDFLoader } from 'langchain/document_loaders/web/pdf';
+    // import { onMount } from 'svelte';
 
-    const url = './src/lib/assets/part_time_phd.pdf';
+    // let pdfjs;
+    // let pdfjsWorker;
+    // const pdfUrl = './src/lib/assets/part_time_phd.pdf';
 
-    prepareDocs(url).then((docs) => {
+    // onMount(async () => {
+    //     pdfjs = await import('pdfjs-dist/legacy/build/pdf.min.mjs');
+    //     pdfjsWorker = await import('pdfjs-dist/legacy/build/pdf.worker.min.mjs');
+    //     pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+    // });
+
+    // export async function prepareDocs(pdfUrl) {
+    //     const pdfResponse = await fetch(pdfUrl);
+    //     const pdfBlob = await pdfResponse.blob();
+    //     const pdfLoader = new WebPDFLoader(pdfBlob);
+    //     const doc = await pdfLoader.load();
+
+    //     return pdfLoader;
+    // }
+    import { prepareDocs } from '$lib/vectorstore';
+
+    const pdfUrl = './src/lib/assets/part_time_phd.pdf';
+
+    prepareDocs(pdfUrl).then((docs) => {
         console.log(docs);
     });
-
-    // (async () => {
-    //     const response = await fetch(url);
-    //     const blob = await response.blob();
-
-    //     const loader = new WebPDFLoader(blob);
-    //     const docs = await loader.load();
-    //     console.log({ docs });
-    // })();
-
-    // console.log({ docs });
-
-    // prepareDocs(url).then((docs) => {
-    //     console.log(docs);
-    // });
 </script>
 
 <p>RAG</p>
